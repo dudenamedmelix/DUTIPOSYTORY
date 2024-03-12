e6cf07f63ebfb459bdc2e9b7da99cdeb9aea929c
Melnychenko Daniil <melixx404@gmail.com>
2024-03-12 15:40:59 +0200

    PR_2 task_1/2/3/4

---
+++ PR_2/task_3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arrayX = new int[16] { 17, 24, 5, 56, 11, 32, 11, 17, 6, 80, 120, 75, 16, 41, 19, 5 };
+
+        Console.WriteLine("Array X:");
+        PrintArray(arrayX);
+
+        int minValue = FindMinValue(arrayX);
+        Console.WriteLine($"Min: {minValue}");
+
+        int maxValue = FindMaxValue(arrayX);
+        Console.WriteLine($"Max: {maxValue}");
+
+        Console.ReadLine();
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int element in arr)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static int FindMinValue(int[] arr)
+    {
+        int minValue = arr[0];
+        foreach (int element in arr)
+        {
+            if (element < minValue)
+            {
+                minValue = element;
+            }
+        }
+        return minValue;
+    }
+
+    static int FindMaxValue(int[] arr)
+    {
+        int maxValue = arr[0];
+        foreach (int element in arr)
+        {
+            if (element > maxValue)
+            {
+                maxValue = element;
+            }
+        }
+        return maxValue;
+    }
+}
---
+++ PR_2/task_4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arrayX = { -3, 7, 0, -5, 12, -8, 6, -10, 4, 9, -2, 13, 14, -12, -17, 16, 17 };
+
+        Console.WriteLine("Array X:");
+        PrintArray(arrayX);
+
+        Console.WriteLine("Enter Module M:");
+        int M = Convert.ToInt32(Console.ReadLine());
+
+        int[] arrayY = GenerateArrayY(arrayX, M);
+        Console.WriteLine($"Module M: {M}");
+        Console.WriteLine("New Array Y:");
+        PrintArray(arrayY);
+
+        Console.ReadLine();
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int element in arr)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+
+
+    static int[] GenerateArrayY(int[] arrX, int M)
+    {
+        int[] arrY = new int[arrX.Length];
+        int yIndex = 0;
+
+        foreach (int element in arrX)
+        {
+            if (Math.Abs(element) > M)
+            {
+                arrY[yIndex++] = element;
+            }
+        }
+
+        Array.Resize(ref arrY, yIndex);
+
+        return arrY;
+    }
+}
